961e113a8de79eb37398ca67de0f2eaaef721a85
CHIN-HUA <tico9004040@gmail.com>
2022-02-14 21:59:36 +0800

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
-
+const fs = require("fs")
+const privateKey = fs.readFileSync(".secret").toString()
 const projectId = ""
 
 module.exports = {
@@ -9,11 +10,11 @@ module.exports = {
     },
     mumbai: {
       url: '',
-      accounts: []
+      accounts: [privateKey]
     },
     mainnet: {
       url: '',
-      accounts: []
+      accounts: [privateKey]
     }
   },
   solidity: "0.8.4",
